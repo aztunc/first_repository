3fcd89906a0e50e4e7057783792bf27b03a6857c
Aziz <aziztunc99@gmail.com>
2022-11-04 15:46:02 +0300

    main

---
+++ app.js
@@ -0,0 +1,11 @@
+var nav = document.querySelector(".nav");
+
+window.addEventListener("scroll",function(){
+    if(window.pageYOffset > 125d0){
+        nav.classList.add("bg-dark","shadow")
+    }
+    else{
+        nav.classList.remove("bg-dark","sh
+})
+
+
